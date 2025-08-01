ab0af68f4b6281f4aac75134e4993e05ca5a0a1d
Amine Hichri <amyn.hichri@gmail.com>
2025-08-01 11:50:24 +0200

    Navigate to the meal details using Navigator

--- lib/screens/meals.dart
+++ lib/screens/meals.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:meals_app/models/meal.dart';
+import 'package:meals_app/screens/meal_details.dart';
 import 'package:meals_app/widgets/meal_item.dart';
 
 class MealsScreen extends StatelessWidget {
@@ -7,11 +8,24 @@ class MealsScreen extends StatelessWidget {
   final String title;
   final List<Meal> meals;
 
+  void selectMeal(BuildContext context, Meal meal) {
+    Navigator.of(context).push(
+      MaterialPageRoute(
+        builder: (ctx) => MealDetailsScreen(meal: meal),
+      ),
+    );
+  }
+
   @override
   Widget build(BuildContext context) {
     Widget content = ListView.builder(
       itemCount: meals.length,
-      itemBuilder: (ctx, index) => MealItem(meal: meals[index]),
+      itemBuilder: (ctx, index) => MealItem(
+        meal: meals[index],
+        onSelectMeal: (meal) {
+          selectMeal(context, meal);
+        },
+      ),
     );
 
     if (meals.isEmpty) {
@@ -19,10 +33,12 @@ class MealsScreen extends StatelessWidget {
           child: Column(
         mainAxisSize: MainAxisSize.min,
         children: [
-           Text('Uh oh ... nothing here!',
-          style: Theme.of(context).textTheme.headlineLarge!.copyWith(
+          Text(
+            'Uh oh ... nothing here!',
+            style: Theme.of(context).textTheme.headlineLarge!.copyWith(
                   color: Theme.of(context).colorScheme.onSurface,
-                ),),
+                ),
+          ),
           const SizedBox(height: 16),
           Text(
             'Try selecting a different category.',
