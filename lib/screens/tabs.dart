ee5b33da46633667d08a97677719beff5afdbe51
Amine Hichri <amyn.hichri@gmail.com>
2025-08-02 17:24:25 +0200

    Managing state and data

--- lib/screens/tabs.dart
+++ lib/screens/tabs.dart
@@ -32,12 +32,12 @@ class _TabsScreenState extends State<TabsScreen> {
       setState(() {
         _favoriteMeals.remove(meal);
       });
-      _showInfoMessage('Meal is no longer a favorites!');
+      _showInfoMessage('Meal is no longer a favorite!');
     } else {
       setState(() {
         _favoriteMeals.add(meal);
       });
-      _showInfoMessage('Marked as a favorites!');
+      _showInfoMessage('Marked as a favorite!');
     }
   }
 
